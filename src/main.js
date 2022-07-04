85dcfa4915e68ddf7e300874b7c3a2b8a8aa4671
KostinNeuro <omroners@gmail.com>
2022-07-04 09:04:08 +0300

    Final version

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { createApp } from 'vue'
+import App from './App'
+// import router from './router'
+// import store from './store'
+import components from './components/UI/';
+import router from "@/router/router";
+import VIntersection from "@/directives/VIntersection";
+import directives from "@/directives";
+import store from "@/store";
+
+
+const app = createApp(App)
+
+
+components.forEach(component => {
+    app.component(component.name, component)
+})
+
+directives.forEach(directive => {
+    app.directive(directive.name, directive)
+
+})
+
+app
+    .use(router)
+    .use(store)
+    .mount('#app')
